568dfac5713c84c0ea924c39e643bcfcf3453cad
“danioc94” <dancol94@gmail.com>
2020-05-25 00:30:30 -0400

    Training one example

--- main.py
+++ main.py
@@ -71,19 +71,30 @@ for im in range(len(resized)):
 
 '''
 print("Hog feats: ", hog_feats)
-print("Hog feats type: ", type(hog_feats))
-print("Hog feats[0]: ", hog_feats[0][0][0])
-print("Hog feats[1]: ", hog_feats[1][0])
+#print("Hog feats type: ", type(hog_feats))
+print("Hog feats[0]: ", hog_feats[0])
+print("Hog feats[0][0][0]: ", hog_feats[0][0][0])
+print("Hog feats[1][0]: ", hog_feats[1][0])
 print("Hog feats[0] type: ", type(hog_feats[0]))
+print("Feature length: ", featureLength)
 '''
+
+featureLength = len(hog_feats[0])
+example = []
+for i in range(len(hog_feats[0])):
+    example.append(hog_feats[0][i][0])
+#print("example: ", example)
 
 # ANN Setup:
 ann = cv2.ml.ANN_MLP_create()
-ann.setLayerSizes(np.array([10, 64, 32, 2], dtype=np.uint8))
+ann.setLayerSizes(np.array([featureLength, 64, 32, 2], dtype=np.uint8))
 ann.setTrainMethod(cv2.ml.ANN_MLP_BACKPROP)
 
+numpyArray = np.array([[1, 1, 1, 1, 0, 1, 1, 0, 0, 1]], dtype=np.float32)
+#print("numpyArray type: ", type(numpyArray))
+
 # ANN Training:
-ann.train(np.array([[1, 1, 1, 1, 0, 1, 1, 0, 0, 1]], dtype=np.float32), cv2.ml.ROW_SAMPLE, np.array([[0, 0, 0, 0, 0, 1, 0, 0, 0]], dtype=np.float32))
+ann.train(np.array([example], dtype=np.float32), cv2.ml.ROW_SAMPLE, np.array([[1, 0]], dtype=np.float32))
 
 # ANN Predict:
 #result = ann.predict(np.array([[1.4, 1.5, 1.2, 2., 2.5, 2.8, 3., 3.1, 3.8]], dtype=np.float32))
