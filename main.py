d9758de0cfee9d1840e8abd6afacbfe0d434e91e
“danioc94” <dancol94@gmail.com>
2020-05-26 22:09:32 -0400

    fixes

--- main.py
+++ main.py
@@ -5,10 +5,10 @@ import numpy as np
 def load_images_from_folder(folder):
     images = []
     for filename in os.listdir(folder):
-        print(filename)
         img = cv2.imread(os.path.join(folder,filename))
         if img is not None:
             images.append(img)
+    print("Loading finised")
     return images
 
 def resize(images):
@@ -22,21 +22,23 @@ def training_data(positives, negatives):
     trainImages = positives + negatives
     trainLables = []
     for i in range(len(trainImages)):
-        if i < 10:
+        if i < 500:
             trainLables.append(1)
         else:
             trainLables.append(-1)
     return trainImages, trainLables
 
-pos_folder = '/home/daniel/Documents/Repositories/VehicleDetector/train/Positive'
+#pos_folder = '/home/daniel/Documents/Repositories/VehicleDetector/train/Positive'
+pos_folder = '/home/daniel/Documents/CarImages/Positive'
 positives = load_images_from_folder(pos_folder)
 
-neg_folder = '/home/daniel/Documents/Repositories/VehicleDetector/train/Negative'
+#neg_folder = '/home/daniel/Documents/Repositories/VehicleDetector/train/Negative'
+neg_folder = '/home/daniel/Documents/CarImages/Negative'
 negatives = load_images_from_folder(neg_folder)
 
 # Training images and labels:
 trainImages, trainLables = training_data(positives, negatives)
-print("Trainlables: ", trainLables)
+#print("Trainlables: ", trainLables)
 
 #Test image:
 test_folder = '/home/daniel/Documents/Repositories/VehicleDetector/test'
@@ -58,7 +60,7 @@ blockStride = (32, 32)
 cellSize = (16, 16)  # h x w in pixels
 nbins = 9  # number of orientation bins
 
-# HOG computation
+# HOG computation for training images:
 hog = cv2.HOGDescriptor(winSize, blockSize, blockStride, cellSize, nbins)
 hog_feats = []
 
@@ -71,8 +73,7 @@ hog_test_feats = []
 for im in range(len(test_resized)):
     im_feature = hog.compute(test_resized[im])
     hog_test_feats.append(im_feature)
-#HOG_test = hog.compute(test_resized)
-print("HOG Test: ", hog_test_feats[0])
+#print("HOG Test: ", hog_test_feats[0])
 
 '''
 print("Hog feats: ", hog_feats)
@@ -92,36 +93,26 @@ for i in range(len(hog_feats[0])):
 
 # Adjusting data for neural net:
 HOG_Data = []
-
 for sample in range(len(hog_feats)):
     HOG_Row = []
     for feature in range(len(hog_feats[sample])):
         HOG_Row.append(hog_feats[sample][feature][0])
         HOG_Data.append(HOG_Row)
+#print("HOG_Data: ", HOG_Data)
 
 # Adjusting test data for neral net:
 HOG_test_Data = []
-
 for sample in range(len(hog_test_feats)):
-    print("sample: ", sample)
     HOG_Row = []
-    print("HOG Row: ", HOG_Row)
     for feature in range(len(hog_test_feats[sample])):
         HOG_Row.append(hog_test_feats[sample][feature][0])
-        #print("HOG Row in: ", HOG_Row)
     HOG_test_Data.append(HOG_Row)
-
-print("HOG test data: ", HOG_test_Data)
-
-#print("HOG_Data: ", HOG_Data)
+#print("HOG test data: ", HOG_test_Data)
 
 # ANN Setup:
 ann = cv2.ml.ANN_MLP_create()
 ann.setLayerSizes(np.array([featureLength, 64, 32, 2], dtype=np.uint8))
 ann.setTrainMethod(cv2.ml.ANN_MLP_BACKPROP)
-
-numpyArray = np.array([[1, 1, 1, 1, 0, 1, 1, 0, 0, 1]], dtype=np.float32)
-#print("numpyArray type: ", type(numpyArray))
 
 # ANN Training:
 for sample in HOG_Data:
