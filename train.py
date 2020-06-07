ac19ebe5293bc22140cf1d83004688e3d90531f2
“danioc94” <dancol94@gmail.com>
2020-06-07 19:13:17 -0400

    Deleting commented code

--- train.py
+++ train.py
@@ -9,7 +9,6 @@ def load_images_from_folder(folder):
         img = cv2.imread(os.path.join(folder,filename))
         if img is not None:
             images.append(img)
-    print("Loading images finised")
     return images
 
 def resize(images):
@@ -37,17 +36,17 @@ def training_data(positives, negatives):
                 trainLables.append([0, 1])
             except:
                 pass
-    print("length lables: ", len(trainLables))
-    #print("Lables: ", trainLables)
     return trainImages, trainLables
 
 #pos_folder = '/home/daniel/Documents/Repositories/VehicleDetector/train/Positive'
 pos_folder = '/home/daniel/Documents/CarImages/Positive'
 positives = load_images_from_folder(pos_folder)
+print("Loading positive images finised")
 
 #neg_folder = '/home/daniel/Documents/Repositories/VehicleDetector/train/Negative'
 neg_folder = '/home/daniel/Documents/CarImages/Negative2'
 negatives = load_images_from_folder(neg_folder)
+print("Loading negative images finised")
 
 # Training images and labels:
 trainImages, trainLables = training_data(positives, negatives)
@@ -59,6 +58,7 @@ resized = resize(trainImages)
 # Test images:
 test_folder = '/home/daniel/Documents/Repositories/VehicleDetector/test'
 test = load_images_from_folder(test_folder)
+print("Loading test images finised")
 test_resized = resize(test)
 
 '''
@@ -87,16 +87,6 @@ hog_test_feats = []
 for im in range(len(test_resized)):
     im_feature = hog.compute(test_resized[im])
     hog_test_feats.append(im_feature)
-#print("HOG Test: ", hog_test_feats[0])
-
-'''
-print("Hog feats: ", hog_feats)
-#print("Hog feats type: ", type(hog_feats))
-print("Hog feats[0]: ", hog_feats[0])
-print("Hog feats[0][0][0]: ", hog_feats[0][0][0])
-print("Hog feats[1][0]: ", hog_feats[1][0])
-print("Hog feats[0] type: ", type(hog_feats[0]))
-'''
 
 # Adjusting data for neural net:
 HOG_Data = []
@@ -105,7 +95,6 @@ for sample in range(len(hog_feats)):
     for feature in range(len(hog_feats[sample])):
         HOG_Row.append(hog_feats[sample][feature][0])
         HOG_Data.append(HOG_Row)
-#print("HOG_Data: ", HOG_Data)
 
 # ANN Setup:
 featureLength = len(hog_feats[0])
@@ -124,12 +113,11 @@ for sample in range(len(hog_test_feats)):
     for feature in range(len(hog_test_feats[sample])):
         HOG_Row.append(hog_test_feats[sample][feature][0])
     HOG_test_Data.append(HOG_Row)
-#print("HOG test data: ", HOG_test_Data)
 
 # ANN Predict:
 for sample in HOG_test_Data:
     result = ann.predict(np.array([sample], dtype=np.float32))
     print("Result: ", result)
 
-# Saving model:
+# Save model:
 ann.save("ann_model")
